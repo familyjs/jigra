72a0eb9d65954da56d0d2efb5d346c2bec008993
NKDuy <nkduy.dev@gmail.com>
2024-06-22 18:20:49 +0700

    chore(ci): Re enable pod validation failures

--- scripts/native-podspec.sh
+++ scripts/native-podspec.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
-export NATIVE_PUBLISH=true
+-set -eo pipefail
 
 case $1 in
      lint)
-       pod spec lint ios/JigraCordova.podspec --allow-warnings
-       pod spec lint ios/Jigra.podspec --allow-warnings;;
+       pod lib lint ios/JigraCordova.podspec --allow-warnings
+       pod lib lint ios/Jigra.podspec --allow-warnings;;
 
      publish)
+       export NATIVE_PUBLISH=true
        pod trunk push ios/JigraCordova.podspec --allow-warnings
        pod trunk push ios/Jigra.podspec --allow-warnings;;
 

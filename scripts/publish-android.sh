97a5a56f187abccf4e4fbc1b593b912af354349d
NKDuy <nkduy.dev@gmail.com>
2024-06-25 22:53:55 +0700

    chore: update ci

--- scripts/publish-android.sh
+++ scripts/publish-android.sh
@@ -11,3 +11,17 @@ mkdir -p ./tmp
 # Export ENV variable used by Gradle for Versioning
 export JIG_VERSION
 export JIG_PUBLISH=true
+
+printf %"s\n" "Publishing $JIG_VERSION to MavenCentral production..."
+
+$DIR/gradlew clean build publish --max-workers 1 -b $DIR/jigra/build.gradle -Pandroid.useAndroidX=true > $LOG_OUTPUT 2>&1
+
+echo $RESULT
+
+if grep --quiet "BUILD SUCCESSFUL" $LOG_OUTPUT; then
+    printf %"s\n" "Success: Jigra Android Library published to MavenCentral."
+else
+    printf %"s\n" "Error publishing, check $LOG_OUTPUT for more info!"
+    cat $LOG_OUTPUT
+    exit 1
+fi
